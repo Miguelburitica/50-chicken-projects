99646c497c875562802a81b398ee21c688cec365
juan-sgs <juan@socialgoodsoftware.com>
2023-08-20 20:50:24 -0500

    Add day4 Juan (chicken)

--- src/components/CenteredContainer.vue
+++ src/components/CenteredContainer.vue
@@ -1,11 +1,17 @@
 <template>
-  <main class="h-screen w-full flex items-center justify-center">
+  <main class="h-screen w-full flex items-center justify-center" :style="color ? { backgroundColor: color } : {}">
     <slot></slot>
   </main>
 </template>
 
 <script>
 export default {
-  name: 'Container'
+  name: 'Container',
+  props: {
+    color: {
+      type: String,
+      default: ''
+    }
+  }
 }
 </script>
---
+++ src/components/challengers/chicken/Day4.vue
@@ -0,0 +1,53 @@
+<template>
+  <CenteredContainer color="cadetblue">
+    <div class="flex">
+      <input :class="inputClass" placeholder="Search..." class="w-0 bg-zinc-100 h-12 text-black outline-none rounded-l-lg"
+        type="text">
+      <button :class="searchClass" @click="toggleSearch" class="flex items-center justify-center h-12 bg-zinc-100 w-12">
+        <IconSearch color="black" size="32" />
+      </button>
+    </div>
+  </CenteredContainer>
+</template>
+
+<script>
+import CenteredContainer from '../../CenteredContainer.vue';
+import { IconSearch } from '@tabler/icons-vue'
+export default {
+  components: {
+    CenteredContainer,
+    IconSearch
+  },
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    toggleSearch() {
+      this.open = !this.open
+    }
+  },
+  computed: {
+    inputClass() {
+      return {
+        'w-64': this.open,
+        'px-4': this.open,
+        'opacity-0': !this.open
+      }
+    },
+    searchClass() {
+      return {
+        'rounded-r-lg': this.open,
+        'rounded-lg': !this.open
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+* {
+  transition: .5s all;
+}
+</style>
